ba5e9593e95ad3331cfcd87307c1f2e23653a7ee
jonasbergmann <jonasbergmann5@googlemail.com>
2018-09-21 00:05:35 +0200

    anfang antwort liste

--- src/data/Runde.java
+++ src/data/Runde.java
@@ -2,6 +2,8 @@ package data;
 
 import database.Database;
 
+import java.util.List;
+
 public class Runde {
 
     private Frage frage;
@@ -9,8 +11,21 @@ public class Runde {
     private Antwort a3;
     private Antwort a4;
 
-    public void getFrage(){
+    public void getFrage(Frage f){
+        this.frage = f;
+        Database d = new Database();
+        List<Antwort> antwortListe = d.getListAllAntworten();
 
+
+        for(int i = 0; i<antwortListe.size(); i++){
+            if(antwortListe.get(i).getAntwort().equals(frage.getAntwort())){
+                i++;
+                }
+            else {
+                a2 = antwortListe.get(i);
+                break;
+            }
+            }
+        }
     }
 
-}
